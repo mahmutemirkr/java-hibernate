b58baf7144dfd879d925d6d6bc69843e8a551690
Mahmut Emir <77195637+mahmutemirkr@users.noreply.github.com>
2022-12-29 15:42:13 +0300

    hb01-annotation-entity-class-RunnerFetch01-SQL-pull-data

--- src/main/java/com/hb01/annotation/RunnerFetch01.java
+++ src/main/java/com/hb01/annotation/RunnerFetch01.java
@@ -5,6 +5,11 @@ import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
 public class RunnerFetch01 {
 
     public static void main(String[] args) {
@@ -27,6 +32,19 @@ public class RunnerFetch01 {
         System.out.println(student01);
         System.out.println(student02);
         System.out.println(student03);
+
+        /*DB operations with SQL*/
+        //SQL
+
+        String sqlQuery = "SELECT * FROM e_student01";
+        List<Objects[]> resultList = session.createSQLQuery(sqlQuery).getResultList();
+
+        System.out.println("-SQL-");
+        for (Object[] objects : resultList){
+
+            System.out.println(Arrays.toString(objects));
+
+        }
 
 
 
