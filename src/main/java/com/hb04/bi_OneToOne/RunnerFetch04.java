98331c7cdc88e4f9ef0cb1032fde3d7c93c93a0c
Mahmut Emir <77195637+mahmutemirkr@users.noreply.github.com>
2022-12-31 17:16:30 +0300

    hb04-annotation-RunnerFetch-OneToOne-InnerJoin-pull-data

--- src/main/java/com/hb04/bi_OneToOne/RunnerFetch04.java
+++ src/main/java/com/hb04/bi_OneToOne/RunnerFetch04.java
@@ -5,6 +5,9 @@ import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class RunnerFetch04 {
 
     public static void main(String[] args) {
@@ -23,6 +26,19 @@ public class RunnerFetch04 {
         System.out.println(student1.getDiary());
         System.out.println("BiDirectional - getStudent");
         System.out.println(diary.getStudent());
+
+
+        //Inner Join
+
+        String hqlQuery = "SELECT s.name, d.name FROM Student04 s INNER JOIN FETCH Diary04 d on s.id=d.student";
+        List<Object[]> resultList = Config.session.createQuery(hqlQuery).getResultList();
+        for (Object[] objects:resultList) {
+
+            System.out.println(Arrays.toString(objects));
+
+        }
+
+
 
 
 
