2e59f97492fed20c73d6ccecf77e64b975b27767
Mahmut Emir <77195637+mahmutemirkr@users.noreply.github.com>
2022-12-31 17:30:54 +0300

    hb04-annotation-RunnerFetch-OneToOne-rightJoin-pull-data

--- src/main/java/com/hb04/bi_OneToOne/RunnerFetch04.java
+++ src/main/java/com/hb04/bi_OneToOne/RunnerFetch04.java
@@ -55,6 +55,14 @@ public class RunnerFetch04 {
         }
 
 
+        //Right Join
+        String hqlQueryRightJoin = "SELECT s.name, d.name FROM Student04 s RIGHT JOIN FETCH Diary04 d on s.id=d.student";
+        List<Object[]> resultListRightJoin = Config.session.createQuery(hqlQueryRightJoin).getResultList();
+        for (Object[] objects:resultListLeftJoin) {
+
+            System.out.println(Arrays.toString(objects));
+
+        }
 
 
 
