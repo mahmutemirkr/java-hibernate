3304cbeb55d3ade31a1869c459ac40c1c1e281f1
Mahmut Emir <77195637+mahmutemirkr@users.noreply.github.com>
2022-12-31 17:58:35 +0300

    hb05-annotation-entity-manyToOne-class-create

---
+++ src/main/java/com/hb05/manyToOne/Student05.java
@@ -0,0 +1,78 @@
+package com.hb05.manyToOne;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+public class Student05 {
+
+    @Id
+    private int id;
+
+    @Column(name="student_name", nullable = false)
+    private String name;
+
+    private int grade;
+
+    private LocalDateTime createOn ;
+
+    @ManyToOne
+    @JoinColumn(name="university_id")
+    private University university;
+
+    @PrePersist
+    public void prePersist(){
+        createOn = LocalDateTime.now();
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    public void setGrade(int grade) {
+        this.grade = grade;
+    }
+
+    public LocalDateTime getCreateOn() {
+        return createOn;
+    }
+
+    public University getUniversity() {
+        return university;
+    }
+
+    public void setUniversity(University university) {
+        this.university = university;
+    }
+
+    // toString()
+
+    @Override
+    public String toString() {
+        return "Student05{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", grade=" + grade +
+                ", createOn=" + createOn +
+                ", university=" + university +
+                '}';
+    }
+
+}
