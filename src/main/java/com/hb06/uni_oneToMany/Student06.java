b527bea7ec35ae67f9d5870ccbdcfd7ec57190f5
Mahmut Emir <77195637+mahmutemirkr@users.noreply.github.com>
2023-01-01 13:33:14 +0300

    hb06-annotation-entity-uni-oneToMany-student-class-create

---
+++ src/main/java/com/hb06/uni_oneToMany/Student06.java
@@ -0,0 +1,66 @@
+package com.hb06.uni_oneToMany;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Student06 {
+
+    @Id
+    private Long id;
+
+    @Column(name = "student_name", nullable = false)
+    private String studentNames;
+
+    private int grade;
+
+    @OneToMany
+    @JoinColumn
+    private List<Book06> bookList = new ArrayList<>();
+
+    //Getter - Setter
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getStudentNames() {
+        return studentNames;
+    }
+
+    public void setStudentNames(String studentNames) {
+        this.studentNames = studentNames;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    public void setGrade(int grade) {
+        this.grade = grade;
+    }
+
+    public List<Book06> getBookList() {
+        return bookList;
+    }
+
+    public void setBookList(List<Book06> bookList) {
+        this.bookList = bookList;
+    }
+
+    @Override
+    public String toString() {
+        return "Student06{" +
+                "id=" + id +
+                ", studentNames='" + studentNames + '\'' +
+                ", grade=" + grade +
+                ", bookList=" + bookList +
+                '}';
+    }
+}
