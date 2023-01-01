e2b78490f6cf3716fff0e945fd9e65ec938d778c
Mahmut Emir <77195637+mahmutemirkr@users.noreply.github.com>
2023-01-01 16:00:24 +0300

    hb06-annotation-entity-uni-oneToMany-RunnerSave-class-create-setData

---
+++ src/main/java/com/hb06/uni_oneToMany/RunnerSave06.java
@@ -0,0 +1,66 @@
+package com.hb06.uni_oneToMany;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+public class RunnerSave06 {
+
+    public static void main(String[] args) {
+
+        Book06 book1 = new Book06();
+        book1.setId(101L);
+        book1.setBookName("Art Book");
+
+        Book06 book2 = new Book06();
+        book2.setId(102L);
+        book2.setBookName("Math Book");
+
+        Book06 book3 = new Book06();
+        book3.setId(103L);
+        book3.setBookName("Java Book");
+
+
+
+        Student06 student1 = new Student06();
+        student1.setId(1001L);
+        student1.setStudentNames("Cedric");
+        student1.setGrade(75);
+        student1.getBookList().add(book1);
+
+        Student06 student2 = new Student06();
+        student2.setId(1002L);
+        student2.setStudentNames("Chen");
+        student2.setGrade(75);
+        student2.getBookList().add(book2);
+
+        Student06 student3 = new Student06();
+        student3.setId(1003L);
+        student3.setStudentNames("Nicolas");
+        student3.setGrade(75);
+        student3.getBookList().add(book3);
+
+
+        Configuration con = new Configuration().configure("hibernate.cfg.xml").
+               addAnnotatedClass(Student06.class).addAnnotatedClass(Book06.class);
+
+        SessionFactory sf = con.buildSessionFactory();
+        Session session = sf.openSession();
+        Transaction tx = session.beginTransaction();
+
+        session.save(book1);
+        session.save(book2);
+        session.save(book3);
+
+        session.save(student1);
+        session.save(student2);
+        session.save(student3);
+
+        tx.commit();
+        session.close();
+        sf.close();
+
+
+    }
+}
