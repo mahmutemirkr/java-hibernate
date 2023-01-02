9efaa019a2b1f3f7aad60bc5ddd78a4ac57152dd
Mahmut Emir <77195637+mahmutemirkr@users.noreply.github.com>
2023-01-02 18:19:52 +0300

    hb07

---
+++ src/main/java/com/hb07/bi_oneToMany/Student07.java
@@ -0,0 +1,63 @@
+package com.hb07.bi_oneToMany;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Student07 {
+
+    @Id
+    private int id;
+
+    @Column(name = "student_name",nullable = false)
+    private String name;
+
+    private int grade;
+
+    @OneToMany(mappedBy = "student",orphanRemoval = true, cascade = CascadeType.REMOVE)
+    private List<Book07> bookList = new ArrayList<>();
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    public void setGrade(int grade) {
+        this.grade = grade;
+    }
+
+    public List<Book07> getBookList() {
+        return bookList;
+    }
+
+    public void setBookList(List<Book07> bookList) {
+        this.bookList = bookList;
+    }
+
+    @Override
+    public String toString() {
+        return "Student07{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", grade=" + grade +
+                ", bookList=" + bookList +
+                '}';
+    }
+
+}
