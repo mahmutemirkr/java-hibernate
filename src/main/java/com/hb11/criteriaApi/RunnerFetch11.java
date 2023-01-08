d53b3aec94a73307f6a317d96df969d583b17c43
Mahmut Emir <77195637+mahmutemirkr@users.noreply.github.com>
2023-01-08 12:52:39 +0300

    hb11-criteriaApi-runnerFetch-class-hql-update

--- src/main/java/com/hb11/criteriaApi/RunnerFetch11.java
+++ src/main/java/com/hb11/criteriaApi/RunnerFetch11.java
@@ -5,6 +5,8 @@ import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
+import java.util.List;
+
 public class RunnerFetch11 {
 
     public static void main(String[] args) {
@@ -21,6 +23,22 @@ public class RunnerFetch11 {
         Student11 student = session.get(Student11.class, 1L);
         student.setName("Updated "+student.getName());
         session.update(student);
+
+
+        //Update -HQL
+    	String hqlQuery1=
+				"UPDATE Student11 s SET s.name='Updated with HQL' where s.id=11";
+		int numOfRec=session.createQuery(hqlQuery1).executeUpdate();
+		System.out.println("Effected Row Count:"+ numOfRec);
+
+		String hqlQuery2="FROM Student11 s WHERE s.mathGrade<50";
+		List<Student11> resultList=
+				session.createQuery(hqlQuery2,Student11.class).getResultList();
+
+		for (Student11 studentX : resultList) {
+			student.setMathGrade(50);
+			session.update(studentX);
+		}
 
 
         tx.commit();
